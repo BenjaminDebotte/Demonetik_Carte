37288eaf0478d24478b1bc3c7bf1d6aca7c5aaaa
bonnemains <bonnemains@ecole.ensicaen.fr>
2016-02-12 13:31:34 +0100

    ajout :verify pin et debit

--- src/com/example/demonetik_carte/HostApduServiceTest.java
+++ src/com/example/demonetik_carte/HostApduServiceTest.java
@@ -3,21 +3,23 @@ package com.example.demonetik_carte;
 import android.nfc.cardemulation.HostApduService;
 import android.os.Bundle;
 import android.util.Log;
-
 import java.util.Arrays;
 
 /**
  * Created by Joan on 02/12/2015.
  */
 public class HostApduServiceTest extends HostApduService {
-    public static final String TAG = "HceTest";
+	public static final String TAG = "HceTest";
 
     protected final static byte[] HCE = StringUtils.convertASCIIStringToByteArray("HCE");
     protected final static byte[] SW_OK = StringUtils.convertHexStringToByteArray("9000");
     protected final static byte[] SW_INS_NOT_SUPPORTED = StringUtils.convertHexStringToByteArray("6D00");
-
+    protected final static byte[] PIN = StringUtils.convertASCIIStringToByteArray("1234");
+    protected final static byte[] SW_PIN_VERIFICATION_NOT_SUCCESSFUL = StringUtils.convertHexStringToByteArray("6300");
+    
     private short counter;
-
+    private int debit_amount;
+    
     public HostApduServiceTest() {
         counter = 0;
     }
@@ -29,13 +31,23 @@ public class HostApduServiceTest extends HostApduService {
         switch(commandApdu[1]) {
             case (byte)0xA4:
                 Log.i(TAG, "this is a select apdu");
-                ret = ConcatArrays(HCE, SW_OK);
+                //ret = ConcatArrays(HCE, SW_OK);
+                ret = SW_OK;
                 break;
             case (byte)0x10:
                 Log.i(TAG, "this is a get counter");
                 byte [] count_resp = convertShortToByteArray(++counter);
                 ret =  ConcatArrays(count_resp, SW_OK);
                 break;
+            case (byte)0x40:  //Debit
+                Log.i(TAG, "This is a Debit apdu");
+            	ret = get_debit_amount(commandApdu);
+            	break;
+            case (byte)0x20:   //PIN
+            	Log.i(TAG,"This is a verify PIN");
+            	ret = verify(commandApdu);
+            	break;
+                
             default:
                 ret = SW_INS_NOT_SUPPORTED;
                 break;
@@ -44,6 +56,23 @@ public class HostApduServiceTest extends HostApduService {
         return ret;
     }
 
+    public byte[] verify(byte[] Apdu){
+    	if(Apdu[4] != 4)
+    		return SW_PIN_VERIFICATION_NOT_SUCCESSFUL;
+    	
+    	if(Apdu[5] == PIN[0] && Apdu[6] == PIN[1] && Apdu[7] == PIN[2] && Apdu[8] == PIN[3])
+    		return SW_OK;
+    	else
+    		return SW_PIN_VERIFICATION_NOT_SUCCESSFUL;
+    }
+    
+    public byte[] get_debit_amount(byte[] Apdu){
+    	debit_amount = Apdu[5];
+    	Log.i(TAG, "montant de :"+ debit_amount);
+    	
+    	return SW_OK;
+    }
+    
     @Override
     public void onDeactivated(int reason) {
 
